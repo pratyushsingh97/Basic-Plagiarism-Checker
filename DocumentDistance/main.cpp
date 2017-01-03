2996f526f3681211f8e0085376e5e0a024c6e43f
pratyushsingh97 <pratyushsingh@utexas.edu>
2017-01-03 14:30:41 -0600

    Initial Commit

---
+++ DocumentDistance/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DocumentDistance
+//
+//  Created by Pratyush Singh on 1/3/17.
+//  Copyright © 2017 Pratyush Singh. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
